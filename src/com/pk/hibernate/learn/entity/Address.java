cc45edb37c538c71b634c92e08578a8f46007d50
Punit Kumar <punit.kumar1989@gmail.com>
2018-09-19 16:33:33 -0400

    Created a Embeddable Entity for User Details. @Embeddeble annotation will tell hibernate not to treat this class as a seperate table.

---
+++ src/com/pk/hibernate/learn/entity/Address.java
@@ -0,0 +1,44 @@
+package com.pk.hibernate.learn.entity;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	private String street;
+	private String city;
+	private String state;
+	private String pinCode;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+
+}
