1fe9c27b68dfc77694018b1944cb49a74055e594
Punit Kumar <punit.kumar1989@gmail.com>
2018-09-24 16:04:40 -0400

    SQL Parameter injecting using the JPA named Parameter way.

--- src/com/pk/hibernate/learn/HibernateTest.java
+++ src/com/pk/hibernate/learn/HibernateTest.java
@@ -22,15 +22,20 @@ public class HibernateTest {
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
+		String minVal="5";
+//		Query<UserDetails> query= session.createQuery("from UserDetails where userId > ?0");// and userName = ?1");
+//		query.setParameter(0, Integer.parseInt(minVal));
+	//	query.setParameter(1, "User 10");
 		
-		Query<String> query= session.createQuery("select userName from UserDetails");
-		query.setFirstResult(5);
-		query.setMaxResults(4);
-		List<String> users = query.getResultList();
+		Query<UserDetails> query= session.createQuery("from UserDetails where userId > :userId and userName = :userName");
+		query.setParameter("userId", Integer.parseInt(minVal));
+		query.setParameter("userName", "User 10");
+		
+		List<UserDetails> users = query.getResultList();
 		session.getTransaction().commit();
 		session.close();
-		for(String u : users){
-			System.out.println("UserName is - "+u);
+		for(UserDetails u : users){
+			System.out.println("UserName is - "+u.getUserName());
 		}
 	}
 }
